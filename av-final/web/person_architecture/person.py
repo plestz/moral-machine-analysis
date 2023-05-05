0bf74d671710ff098cdc064e860733e6de54b9c7
plestz <96513494+plestz@users.noreply.github.com>
2023-05-05 17:43:58 +0000

    switched region and decision making to smart car

--- av-final/web/person_architecture/person.py
+++ av-final/web/person_architecture/person.py
@@ -1,5 +1,16 @@
+from enum import Enum
+
+class Class(Enum):
+    IMPOVERISHED = 0
+    LOWER = 1
+    LOWER_MIDDLE = 2
+    MIDDLE = 3
+    UPPER_MIDDLE = 4
+    UPPER = 5
+    ELITE = 6
+
 class Person:
-    def __init__(self, name: str, age: int, gender: str, weight: float, education_level: int, occupation: str, is_doctor: bool, married: bool, num_children: int, num_living_relatives: int, actively_breaking_law: bool):
+    def __init__(self, name: str, age: int, gender: str, weight: float, education_level: int, occupation: str, is_doctor: bool, employed: bool, wealth_class: Class, married: bool, num_children: int, num_living_relatives: int, actively_breaking_law: bool):
         self.name = name
         self.age = age
         self.gender = gender
@@ -8,6 +19,7 @@ class Person:
         self.occupation = occupation
         self.is_doctor = is_doctor
         self.employed = employed
+        self.wealth_class = wealth_class # CONTROVERSIAL
         # self.net_worth = net_worth
         # self.societal_impact = societal_impact
         # self.nationality = nationality
@@ -17,14 +29,4 @@ class Person:
         self.num_living_relatives = num_living_relatives
         self.actively_breaking_law = actively_breaking_law
 
-    def calculate_value(self) -> int:
-        return (100 - self.age) \
-                + (300 - int(self.weight))
-                + (200 + self.education_level * 10)
-                + (400 if self.is_doctor else 0)
-                + (300 if self.employed else 0)
-                + (200 if self.married else 0)
-                + (100 * self.num_children)
-                + (20 * self.num_living_relatives)
-                + (-400 if actively_breaking_law else 0)
-
+    
--- av-final/web/vehicle_architecture/smart_car.py
+++ av-final/web/vehicle_architecture/smart_car.py
@@ -1,15 +1,21 @@
 from enum import Enum
 from "av-final/web/person_architecture" import Passenger, Pedestrian, Person
 
+class Region(Enum):
+    WESTERN = 0
+    EASTERN = 1
+    SOUTHERN = 2
+
 class Decision(Enum):
     PROTECT_PASSENGERS = 0
     PROTECT_PEDESTRIANS = 1
 
 class SmartCar(Car):
-    def __init__(self):
+    def __init__(self, region: Region):
         super().__init__()
         self.activate_engine()
         self.activate_computer()
+        self.region = region
         self.vehicle_on = True
 
     def activate_computer(self):
@@ -21,10 +27,10 @@ class SmartCar(Car):
         return self.calculate_mean_decision(passengers, pedestrians)
 
     def calculate_mean_decision(self, passengers: "list[Passenger]", pedestrians: "list[Pedestrian]"):
-        tot_passenger_value = sum([calculate_value(passenger) for passenger in passengers])
+        tot_passenger_value = sum([self.calculate_value(passenger) for passenger in passengers])
         avg_passenger_value = tot_passenger_value / len(passengers)
 
-        tot_pedestrian_value = sum([calculate_value(pedestrian) for pedestrian in pedestrian])
+        tot_pedestrian_value = sum([self.calculate_value(pedestrian) for pedestrian in pedestrian])
         avg_pedestrian_value = tot_pedestrian_value / len(pedestrian)
 
         return PROTECT_PASSENGERS 
@@ -37,6 +43,30 @@ class SmartCar(Car):
         v_pedestrians: "list[Pedestrian]" = pedestrians if pedestrians else self.survey_environment()
 
         return v_passengers, v_pedestrians
+
+    def calculate_value(self, person: Person) -> int:
+        return ((100 - self.age) if self.age >= 30 else (self.age + 20)) \
+                + (300 - int(self.weight))
+                + (200 + self.education_level * 10)
+                + (400 if self.is_doctor else 0)
+                + (300 if self.employed else 0)
+                + (200 if self.married else 0)
+                + (100 * self.num_children)
+                + (20 * self.num_living_relatives)
+                + (-400 if actively_breaking_law else 0)
+
+    def get_regional_bias(self):
+        regional_age_bias = None
+        regional_weight_bias = None
+        regional_weight_bias = None
+
+        match self.region:
+            case Region.WESTERN:
+
+            case Region.EASTERN:
+            case Region.SOUTHERN:
+
+
 
 
     def activate_engine(self): # Implementation irrelevant to project
